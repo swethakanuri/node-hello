9cb775d181d17d6519fd470dfff0ad8d344f5b2e
Kanuri Swetha <kanuri.10741827@ltimindtree.com>
2024-06-14 10:20:09 +0530

    new version 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node 123456789!\n'
+  const msg = 'Hello Node 12345678900!\n'
   res.end(msg);
 });
 
